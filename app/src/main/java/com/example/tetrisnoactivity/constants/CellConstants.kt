c9a14c439720b110fe6bdc9315d2b9058e2e4718
glassnekeep <56785261+glassnekeep@users.noreply.github.com>
2021-11-17 13:54:59 +0300

    Initial commit

---
+++ app/src/main/java/com/example/tetrisnoactivity/constants/CellConstants.kt
@@ -0,0 +1,5 @@
+package com.example.tetrisnoactivity.constants
+
+enum class CellConstants(val value: Byte) {
+    EMPTY(0), EPHEMERAL(1)
+}
---
+++ app/src/main/java/com/example/tetrisnoactivity/helpers/HelperFunctions.kt
@@ -0,0 +1,5 @@
+package com.example.tetrisnoactivity.helpers
+
+fun array2dOfByte(sizeOuter: Int, sizeInner: Int): Array<ByteArray> = Array(sizeOuter) {
+    ByteArray(sizeInner)
+}
